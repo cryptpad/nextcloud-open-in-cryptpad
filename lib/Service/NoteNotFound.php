a07fdb6561afc8051b359b8363c532261fa77653
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-01-16 14:49:11 +0100

    Remove example code

--- lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Wolfgang Ginolas <wolfgang.ginolas@xwiki.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\OpenInCryptpad\Service;
-
-class NoteNotFound extends \Exception {
-}
