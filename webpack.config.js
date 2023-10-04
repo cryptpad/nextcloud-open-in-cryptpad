5e59eeb1b26c3e40e943bb5029893082f9da0ff2
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-10-04 10:25:01 +0200

    Add Nextcloud dialogs and fix CSP and chunking problem

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const webpack = require('webpack')
 
-webpackConfig.entry.settings = path.resolve(path.join('src', 'settings.js'));
-webpackConfig.entry.editor = path.resolve(path.join('src', 'editor.js'));
+webpackConfig.entry.settings = path.resolve(path.join('src', 'settings.js'))
+webpackConfig.entry.editor = path.resolve(path.join('src', 'editor.js'))
+
+webpackConfig.plugins.push(new webpack.optimize.LimitChunkCountPlugin({
+	maxChunks: 1
+}))
 
 module.exports = webpackConfig
