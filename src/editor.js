1eb50ee24bb0dfe8b65cd3be4f9d7650465eccb9
zuzanna-maria <zuzanna.ludzik@gmail.com>
2024-10-17 14:53:56 +0200

    Changed way in which images are requested upon upload

--- src/editor.js
+++ src/editor.js
@@ -8,7 +8,6 @@ import { saveFileContent, deferredToPromise } from './utils.js'
 import { getRequestToken } from '@nextcloud/auth'
 
 import '@nextcloud/dialogs/style.css'  // eslint-disable-line
-import { isBuffer } from 'lodash'
 
 __webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
 __webpack_public_path__ = generateFilePath('openincryptpad', '', 'js/') // eslint-disable-line
@@ -94,27 +93,8 @@ async function onInsertImage(data, callback) {
 		.build()
 
 	const path = await filepicker.pick()
-
-	
-	// const fileClient = OC.Files.getClient()
-	// const contents = (await deferredToPromise(fileClient.getFileContents(path)))[1]
-	// 'invalid or missing file' error
-	const blob = await loadFileContent(path, 'image/png')
-	
-	
-	// const shares = await getShares(path, false)
-	// const shareUrl = findShareUrl(shares)
-	// direct link - does not work
-	// const blob = await getImage('http://localhost:8080/index.php/f/113')
-
-	//	example image binary data - this works?
-	// 	var data = new Uint8Array([
-	//   137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,8,0,0,
-	//   0,8,8,2,0,0,0,75,109,41,220,0,0,0,34,73,68,65,84,8,215,99,120,
-	//   173,168,135,21,49,0,241,255,15,90,104,8,33,129,83,7,97,163,136,
-	//   214,129,93,2,43,2,0,181,31,90,179,225,252,176,37,0,0,0,0,73,69,
-	//   78,68,174,66,96,130]);
-	// 	var blob = new Blob([data], { type: "image/png" });
+	const fileClient = OC.Files.getClient()
+	const blob = await getImage(fileClient._buildUrl(path))
 
 	callback({ blob }) // eslint-disable-line n/no-callback-literal
 }
