d834190990799c9e44a440f1f37e03f4774825f0
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2025-02-17 15:50:00 +0100

    Fix CSP nonce for older versions

--- templates/editor.php
+++ templates/editor.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 
 script('openincryptpad', 'openincryptpad-editor');
 style('openincryptpad', 'editor');
+
+if ($_['cspNonce']) {
+	$nonce = $_['cspNonce'];  // For Nextcloud version >= 30
+} else {
+	$nonce = base64_encode($_['requesttoken']);  // For Nextcloud version < 30
+}
 ?>
 
-<script nonce="<?php p($_['cspNonce']) ?>"><?php print_unescaped($_['infoScript']) ?></script>
+<script nonce="<?php p($nonce) ?>"><?php print_unescaped($_['infoScript']) ?></script>
 
-<script nonce="<?php p($_['cspNonce']) ?>"
+<script nonce="<?php p($nonce) ?>"
 	src="<?php p($_["apiUrl"]) ?>" type="text/javascript"></script>
 <div id="unsaved-indicator">
 	<?php p($l->t('Saving...')); ?>
