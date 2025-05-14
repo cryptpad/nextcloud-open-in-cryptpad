623c615541dd24ddc15da654e65d0dfbf41eaa0a
AAAMON <andreeapetcu@protonmail.com>
2025-05-14 12:06:57 +0300

    Add missing German (formal) translations

---
+++ l10n/de_DE.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+  "openincryptpad",
+  {
+	"Open in CryptPad": "Mit CryptPad bearbeiten",
+	"Error while opening file": "Fehler beim öffnen der Datei",
+	"The CryptPad instance is not configured correctly. Please contact your admin.": "Die CryptPad-Instanz ist nicht korrekt konfiguriert. Bitte kontaktieren Sie Ihren Admin.",
+	"Pick an image": "Bitte wählen Sie ein Bild",
+	"New diagrams.net diagram": "Neues diagrams.net Diagramm",
+	"diagram.drawio": "diagramm.drawio",
+	"File could not be created": "Die Datei konnte nicht angelegt werden",
+},
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
