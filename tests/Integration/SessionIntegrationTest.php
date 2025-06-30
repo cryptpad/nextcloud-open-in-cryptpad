4e3845de7ec4f853354e39c8f67acbcee569e162
Wolfgang Ginolas <wolfgang@cryptpad.org>
2025-06-30 14:21:20 +0200

    Update SPDX headers

--- tests/Integration/SessionIntegrationTest.php
+++ tests/Integration/SessionIntegrationTest.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-// SPDX-FileCopyrightText: Wolfgang Ginolas <wolfgang.ginolas@xwiki.com>
+// SPDX-FileCopyrightText: XWiki CryptPad Team <contact@cryptpad.org> and contributors
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 namespace OCA\OpenInCryptPad\Tests\Integration;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Wolfgang Ginolas <wolfgang.ginolas@xwiki.com>
+// SPDX-FileCopyrightText: XWiki CryptPad Team <contact@cryptpad.org> and contributors
 // SPDX-License-Identifier: AGPL-3.0-or-later
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
